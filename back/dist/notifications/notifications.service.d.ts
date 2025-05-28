50e4468c0801ab12f75af841eeae6e2a7778633b
Manikantaaa <arigelamanikanta79@gmail.com>
2025-05-28 17:31:12 +0530

    back

---
+++ back/dist/notifications/notifications.service.d.ts
@@ -0,0 +1,13 @@
+import { PrismaService } from '../prisma/prisma.service';
+import { Notification } from '@prisma/client';
+export declare class NotificationsService {
+    private prisma;
+    constructor(prisma: PrismaService);
+    create(data: {
+        title: string;
+        description: string;
+        image?: string;
+    }): Promise<Notification>;
+    findAll(): Promise<Notification[]>;
+    findOne(id: number): Promise<Notification>;
+}
